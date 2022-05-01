ac395d83628f31ff561476c1eda8e90f7381aae9
glapi <abhazim.nurbat@gmail.com>
2022-05-01 16:11:01 +0200

    Model mapper

--- src/main/java/com/blog/api/ApiApplication.java
+++ src/main/java/com/blog/api/ApiApplication.java
@@ -1,7 +1,9 @@
 package com.blog.api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -10,4 +12,8 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
