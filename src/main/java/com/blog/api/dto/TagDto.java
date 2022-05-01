2677a9d53a5461a44b503c6b8899f02679cae223
glapi <abhazim.nurbat@gmail.com>
2022-05-01 20:27:56 +0200

    JsonIgnore posts

--- src/main/java/com/blog/api/dto/TagDto.java
+++ src/main/java/com/blog/api/dto/TagDto.java
@@ -2,8 +2,10 @@ package com.blog.api.dto;
 
 import com.blog.api.entity.Tag;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 
 import java.time.LocalDateTime;
 import java.util.Set;
