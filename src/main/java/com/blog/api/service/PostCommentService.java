aeaa2a1feacd242faaa50c7ee1bcd61531e5db3d
glapi <abhazim.nurbat@gmail.com>
2022-05-01 18:39:55 +0200

    Create comment

--- src/main/java/com/blog/api/service/PostCommentService.java
+++ src/main/java/com/blog/api/service/PostCommentService.java
@@ -3,6 +3,7 @@ package com.blog.api.service;
 import com.blog.api.entity.PostComment;
 
 public interface PostCommentService {
+    PostComment create(PostComment postComment);
     void delete(Long id);
     PostComment update(Long commentId, PostComment postComment);
 }
