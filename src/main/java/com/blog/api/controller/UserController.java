9ada1008d94e0e70345395a988523ffe428d74a9
glapi <abhazim.nurbat@gmail.com>
2022-05-01 20:35:46 +0200

    Get user posts and comments

--- src/main/java/com/blog/api/controller/UserController.java
+++ src/main/java/com/blog/api/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.blog.api.controller;
 
+import com.blog.api.dto.PostCommentDto;
+import com.blog.api.dto.PostDto;
 import com.blog.api.dto.UserDto;
+import com.blog.api.entity.Post;
+import com.blog.api.entity.PostComment;
 import com.blog.api.entity.User;
+import com.blog.api.service.impl.IPostCommentService;
+import com.blog.api.service.impl.IPostService;
 import com.blog.api.service.impl.IUserService;
 import com.blog.api.utils.JWTUtil;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -21,6 +28,10 @@ public class UserController {
     private final IUserService iUserService;
     private final AuthenticationManager authManager;
     private final JWTUtil jwtUtil;
+    private final IPostService iPostService;
+    private final IPostCommentService iPostCommentService;
+    private final PostDto postDto = new PostDto();
+    private final PostCommentDto postCommentDto = new PostCommentDto();
 
     @PostMapping("/registration")
     public ResponseEntity<UserDto> registration(@RequestBody UserDto userDto,
@@ -44,5 +55,23 @@ public class UserController {
         User user = iUserService.getUserByUsername(username);
         String jwt = jwtUtil.generateToken(user);
         return ResponseEntity.ok(jwt);
+    }
+
+    @GetMapping("/{username}/posts")
+    public ResponseEntity<List<PostDto>> getUserPosts(@PathVariable("username") String username) {
+        List<Post> posts = iPostService.getUserPosts(username);
+        return ResponseEntity.ok(posts
+                                 .stream()
+                                 .map(postDto::convertToDto)
+                                 .toList());
+    }
+
+    @GetMapping("/{username}/comments")
+    public ResponseEntity<List<PostCommentDto>> getUserComments(@PathVariable("username") String username) {
+        List<PostComment> comments = iPostCommentService.getUserComments(username);
+        return ResponseEntity.ok(comments
+                .stream()
+                .map(postCommentDto::convertToDto)
+                .toList());
     }
 }
