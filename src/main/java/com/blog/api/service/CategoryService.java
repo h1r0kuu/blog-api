3efc51ae885e15f1cda910283f6ce67b765b53d8
glapi <abhazim.nurbat@gmail.com>
2022-05-01 16:52:05 +0200

    Create category method

--- src/main/java/com/blog/api/service/CategoryService.java
+++ src/main/java/com/blog/api/service/CategoryService.java
@@ -1,4 +1,7 @@
 package com.blog.api.service;
 
+import com.blog.api.entity.Category;
+
 public interface CategoryService {
+    Category create(Category category);
 }
--- src/main/java/com/blog/api/service/impl/ICategoryService.java
+++ src/main/java/com/blog/api/service/impl/ICategoryService.java
@@ -1,8 +1,20 @@
 package com.blog.api.service.impl;
 
+import com.blog.api.dao.CategoryRepository;
+import com.blog.api.entity.Category;
 import com.blog.api.service.CategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ICategoryService implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public Category create(Category category) {
+        Category createdCategory = categoryRepository.save(category);
+        return createdCategory;
+    }
 }
